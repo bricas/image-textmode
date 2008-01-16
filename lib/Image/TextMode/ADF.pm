8fcd52435786a82e81cad3bfda839114eb6779a1
Brian Cassidy <bricas@cpan.org>
2008-01-16 03:23:20 +0000

    adf work

--- lib/Image/TextMode/ADF.pm
+++ lib/Image/TextMode/ADF.pm
@@ -9,47 +9,17 @@ Image::TextMode::ADF - Load, create, manipulate and save ADF image files
 	use Image::TextMode::ADF;
 
 	# Read in a file...
-	my $img = Image::TextMode::ADF->new( file => 'myADF.xb' );
-
-	# Image width and height
-	my $w = $img->width;
-	my $h = $img->height;
-
-	# get and put "pixels"
-	my $pixel = $img->getpixel( $x, $y );
-	$img->putpixel( $x, $y, $pixel );
-
-	# font (ADF::Font)
-	my $font = $img->font;
-
-	# palette (ADF::Palette)
-	my $palette = $img->palette;
-
-	# save the data to a file
-	$img->write( file => 'x.xb' );
+	my $img = Image::TextMode::ADF->new;
+    $img->read( { file => 'my.adf' } );
 
 =head1 DESCRIPTION
 
-ADF stands for "eXtended BIN" -- an extention to the normal raw-image BIN files.
-
-ADF features:
-
-=over 4 
-
-=item * allows for binary images up to 65536 columns wide, and 65536 lines high
-
-=item * can have an alternate set of palette colors either in blink or in non-blink mode
-
-=item * can have different textmode fonts from 1 to 32 scanlines high, consisting of either 256 or 512 different characters
-
-=item * can be compressed
-
-=back
+ADF stands for "Artworx Data Format"
 
 ADF file stucture:
 
 	+------------+
-	| Header     |
+	| Version    |
 	+------------+
 	| Palette    |
 	+------------+
@@ -57,9 +27,6 @@ ADF file stucture:
 	+------------+
 	| Image Data |
 	+------------+
-
-Note, the only required element is a header. See the ADF specs for for information.
-http://www.acid.org/info/xbin/xbin.htm
 
 =head1 INSTALLATION
 
@@ -75,37 +42,16 @@ use base qw( Image::TextMode );
 use strict;
 use warnings;
 
-use Carp;
-use Image::TextMode::ADF::Parser;
+use Image::TextMode::Palette;
+use Image::TextMode::Font;
 
 our $VERSION = '0.01';
 
 __PACKAGE__->mk_classaccessors( 'version' );
 
+my @color_idx = (0,1,2,3,4,5,20,7,56,57,58,59,60,61,62,63 );
+
 =head1 METHODS
-
-=head2 new( %options )
-
-Creates a new ADF image. Currently only reads in data.
-
-	# filename
-	$ADF = Image::TextMode::ADF->new( file => 'file.xb' );
-	
-	# file handle
-	$ADF = Image::TextMode::ADF->new( handle => $handle );
-
-	# string
-	$ADF = Image::TextMode::ADF->new( string => $string );
-
-=cut
-
-sub new {
-    my $class = shift;
-    my $args  = ( @_ == 1 && ref $_[ 0 ] eq 'HASH' ) ? $_[ 0 ] : { @_ };
-    my $self  = bless $args, $class;
-    $self->clear;
-    return $self;
-}
 
 =head2 clear(  )
 
@@ -121,17 +67,56 @@ sub clear {
     $self->SUPER::clear( @_ );
 }
 
-=head2 read( %options )
+=head2 parse( %options )
 
-Explicitly reads in an ADF.
+Reads in an ADF.
 
 =cut
 
 sub parse {
     my $self = shift;
+    my ( $file, %options ) = @_;
 
-    require Image::TextMode::ADF::Parser;
-    Image::TextMode::ADF::Parser->parse( $self, @_ );
+    seek( $file, 0, 0 );
+
+    my $version;
+    $file->read( $version, 1 );
+    $version = unpack('C', $version);
+    $self->version( $version );
+
+    my $paldata;
+    $file->read( $paldata, 192 );
+
+    my @pal = unpack( 'C*', $paldata );
+    my @colors;
+    for( @color_idx ) {
+        my $offset = $_ * 3;
+        push @colors, [ map { $_ << 2  } @pal[ $offset..$offset+2 ] ],
+    }
+
+    $self->palette(
+        Image::TextMode::Palette->new( { colors => \@colors } ) );
+
+    my $fontdata;
+    $file->read( $fontdata, 4096 );
+
+    $self->font(
+        Image::TextMode::Font->new_from_raw_data( $fontdata, 16 )
+    );
+
+    my ( $x, $y ) = ( 0, 0 );
+    my $chardata;
+    while ( $file->read( $chardata, 2 ) ) {
+        my @data = unpack( 'aC', $chardata );
+        last if $data[ 0 ] eq chr( 26 );
+        $self->putpixel( $x, $y, @data );
+
+        $x++;
+        if ( $x == 80 ) {
+            $x = 0;
+            $y++;
+        }
+    }
 
     return $self;
 }
@@ -141,44 +126,20 @@ sub parse {
 Returns the ADF data as a string - suitable for saving.
 
 =cut
+
 sub as_string {
-
-=pod
-
     my $self = shift;
 
     my $output;
 
-# must set header to uncompressed because we don't have a compression alg yet.
-# set old value back when done.
-# this is temporary!!!
-    my $compressed = $self->is_compressed;
-    $self->compress( 0 );
+    $output .= pack( 'C', $self->version );
+    $output .= $self->palette->as_string;
+    $output .= $self->font->as_string;
 
-    # header
-    $output .= pack( $header_template, map { $self->$_ } @header_fields );
-
-    # palette
-    if ( $self->has_palette ) {
-        $output .= $self->palette->as_string;
-    }
-
-    # font
-    if ( $self->has_font ) {
-        $output .= $self->font->as_string;
-    }
-
-    # image
-    if ( $self->is_compressed ) {
-
-        # RLE compression alg.
-    }
-    else {
-        for my $y ( 0 .. $self->height - 1 ) {
-            for my $x ( 0 .. $self->width - 1 ) {
-                my $pixel = $self->getpixel( $x, $y );
-                $output .= pack( 'C*', ord( $pixel->char ), $pixel->attr );
-            }
+    for my $y ( 0 .. $self->height - 1 ) {
+        for my $x ( 0 .. $self->width - 1 ) {
+            my $pixel = $self->getpixel( $x, $y );
+            $output .= pack( 'C*', ord( $pixel->char ), $pixel->attr );
         }
     }
 
@@ -186,44 +147,16 @@ sub as_string {
         $output .= $self->sauce->as_string;
     }
 
-    # set old value
-    $self->compress( $compressed );
-
     return $output;
-
-=cut
-
 }
-
-=head2 width( )
-
-Returns the image width.
-
-=head2 height( )
-
-Returns the image height.
-
-=head1 TODO
-
-=over 4
-
-=item * fix write() method to include compression
-
-=item * use new()'s options to create a new file from scratch
-
-=back
 
 =head1 AUTHOR
 
-=over 4 
-
-=item * Brian Cassidy E<lt>bricas@cpan.orgE<gt>
-
-=back
+Brian Cassidy E<lt>bricas@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2005 by Brian Cassidy
+Copyright 2007 by Brian Cassidy
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
