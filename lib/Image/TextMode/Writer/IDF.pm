fdb9a354ac6f94017309bf486bc66264f6b4cb5a
Brian Cassidy <bricas@cpan.org>
2010-01-01 14:56:21 -0400

    prep release

--- lib/Image/TextMode/Writer/IDF.pm
+++ lib/Image/TextMode/Writer/IDF.pm
@@ -50,7 +50,7 @@ Brian Cassidy E<lt>bricas@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2008-2009 by Brian Cassidy
+Copyright 2008-2010 by Brian Cassidy
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
