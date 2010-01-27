af0b61a7cce9a7181b00bd5afda51676f2e881cb
Brian Cassidy <bricas@cpan.org>
2010-01-27 11:34:09 -0400

    perltidy and ignore .bak files in scm

--- corpus/render_corpus.pl
+++ corpus/render_corpus.pl
@@ -6,74 +6,61 @@ use lib "$FindBin::Bin/../lib";
 use Image::TextMode::Renderer::GD;
 
 my @corpus = (
-    {
-        input  => '42_WOLVR.XB',
+    {   input  => '42_WOLVR.XB',
         class  => 'XBin',
         output => 'xbin',
     },
-    {
-        input  => '0994MEMB.CIA',
-        class  => 'ANSI',
-        output => 'ans_ced',
+    {   input      => '0994MEMB.CIA',
+        class      => 'ANSI',
+        output     => 'ans_ced',
         renderopts => { ced => 1 },
     },
-    {
-        input  => 'AVE-AMI1.ASC',
-        class  => 'ANSI',
-        output => 'asc_amiga',
+    {   input      => 'AVE-AMI1.ASC',
+        class      => 'ANSI',
+        output     => 'asc_amiga',
         renderopts => { '_font' => 'Amiga' },
     },
-    {
-        input  => 'bjasc101.asc',
+    {   input  => 'bjasc101.asc',
         class  => 'ANSI',
         output => 'asc',
     },
-    {
-        input  => 'dy-reviv.bin',
+    {   input  => 'dy-reviv.bin',
         class  => 'Bin',
         output => 'bin',
     },
-    {
-        input  => 'HU-ERMAG.BIN',
-        class  => 'Bin',
-        readopts => { width => 202 },
-        output => 'bin_icecolor',
+    {   input      => 'HU-ERMAG.BIN',
+        class      => 'Bin',
+        readopts   => { width => 202 },
+        output     => 'bin_icecolor',
         renderopts => { icecolor => 1 },
     },
 
-    {
-        input  => 'JD-CH01.ANS',
+    {   input  => 'JD-CH01.ANS',
         class  => 'ANSIMation',
         output => 'ansimation',
     },
-    {
-        input  => 'LD-AF1.ANS',
+    {   input  => 'LD-AF1.ANS',
         class  => 'ANSI',
         output => 'ans',
     },
-    {
-        input  => 'LD-AF1.ANS',
-        class  => 'ANSI',
-        output => 'ans_9th',
+    {   input      => 'LD-AF1.ANS',
+        class      => 'ANSI',
+        output     => 'ans_9th',
         renderopts => { '9th_bit' => 1 }
     },
-    {
-        input  => 'MD-XMAS.IDF',
+    {   input  => 'MD-XMAS.IDF',
         class  => 'IDF',
         output => 'idf',
     },
-    {
-        input  => 'RA-ROUTE.ANS',
+    {   input  => 'RA-ROUTE.ANS',
         class  => 'ANSI',
         output => 'ans_80x50',
     },
-    {
-        input  => 'US-NCPL1.ADF',
+    {   input  => 'US-NCPL1.ADF',
         class  => 'ADF',
         output => 'adf',
     },
-    {
-        input  => 'zv_steve.tnd',
+    {   input  => 'zv_steve.tnd',
         class  => 'Tundra',
         output => 'tnd',
     },
@@ -84,7 +71,7 @@ my $renderer = Image::TextMode::Renderer::GD->new;
 for my $work ( @corpus ) {
     my $class = 'Image::TextMode::Format::' . $work->{ class };
     eval "require $class;";
-    
+
     printf "Rendering %s as %s ...\n", $work->{ input }, $work->{ output };
 
     my $input = $class->new;
@@ -92,7 +79,7 @@ for my $work ( @corpus ) {
 
     my $renderopts = $work->{ renderopts } || {};
 
-    if( my $font = delete $renderopts->{ '_font' } ) {
+    if ( my $font = delete $renderopts->{ '_font' } ) {
         $font = 'Image::TextMode::Font::' . $font;
         eval "require $font;";
         $input->font( $font->new );
